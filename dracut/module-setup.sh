70e965367ebe28deb2814218a32f5a2a9a2989e7
Alexander Larsson <alexl@redhat.com>
2024-04-19 10:35:30 +0200

    dracut: For now, don't remove bash/vi as it caused some errors
    We can later look at minimizing

--- dracut/module-setup.sh
+++ dracut/module-setup.sh
@@ -3,11 +3,4 @@
 install() {
     inst /usr/lib/ostree/ostree-prepare-root
     inst /usr/lib/ostreeinit/ostreeinit /init
-
-    # Don't install bash
-    > "${initdir}/usr/bin/bash"
-
-    # Don't install vi and various deps
-    > "${initdir}/usr/libexec/vi"
 }
-
